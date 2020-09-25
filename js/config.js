e8ce80176baa137224e0abb52d42a68e8433b4cd
posmarket <steve@posmarket.com.au>
2020-09-25 13:16:14 +1000

    changes to config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
+// use this page to initiate cognito login
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_9z38Wa0RB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3bjlk1rp3pm9eprpcthp6ka4hu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
